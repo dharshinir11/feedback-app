84f03bff7457545373fc1cc815bf20e84d24d886
Ridhyadharshinni <ridhyatr@gmail.com>
2025-09-17 21:33:23 +0530

    Add files via upload

---
+++ src/data/questions.js
@@ -0,0 +1,9 @@
+export default {
+  Q1: "Opinion about overall session",
+  Q2: "Clarity in the speech",
+  Q3: "Speaker's interaction with the students",
+  Q4: "Did the session deliver the information you expected to receive",
+  Q5: "Was the subject matter presented effectively",
+  Q6: "Rate the speaker's knowledge",
+  Q7: "Suggestions/Questions/Feedback"
+};
