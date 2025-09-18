b267134b9d11d0c509ab3382eea9cd973bb1b178
Ridhyadharshinni <ridhyatr@gmail.com>
2025-09-19 01:10:16 +0530

    Add files via upload

---
+++ src/data/sessionData.js
@@ -0,0 +1,110 @@
+const sessionData = 
+{
+  "CSE": {
+    "16.09.2025": [
+      {
+        "time": "09:00-13:00",
+        "topic": "Smart India Hackathon",
+        "venue": "Open Auditorium"
+      }
+    ]
+  },
+  "IT": {
+    "16.09.2025": [
+      {
+        "time": "09:00-13:00",
+        "topic": "Smart India Hackathon",
+        "venue": "Open Auditorium"
+      }
+    ]
+  },
+  "ECE": {
+    "16.09.2025": [
+      {
+        "time": "09:00-13:00",
+        "topic": "Smart India Hackathon",
+        "venue": "Open Auditorium"
+      }
+    ]
+  },
+  "MECHATRONICS": {
+    "16.09.2025": [
+      {
+        "time": "09:00-13:00",
+        "topic": "Smart India Hackathon",
+        "venue": "Open Auditorium"
+      }
+    ]
+  },
+  "EEE": {
+    "16.09.2025": [
+      {
+        "time": "09:00-13:00",
+        "topic": "Smart India Hackathon",
+        "venue": "Open Auditorium"
+      }
+    ]
+  },
+  "CSBS": {
+    "16.09.2025": [
+      {
+        "time": "09:00-13:00",
+        "topic": "Smart India Hackathon",
+        "venue": "Open Auditorium"
+      }
+    ]
+  },
+  "CIVIL": {
+    "16.09.2025": [
+      {
+        "time": "09:00-13:00",
+        "topic": "Smart India Hackathon",
+        "venue": "Open Auditorium"
+      }
+    ]
+  },
+  "AMCS": {
+    "16.09.2025": [
+      {
+        "time": "09:00-13:00",
+        "topic": "Smart India Hackathon",
+        "venue": "Open Auditorium"
+      }
+    ]
+  },
+  "MECH": {
+    "16.09.2025": [
+      {
+        "time": "09:00-13:00",
+        "topic": "Smart India Hackathon",
+        "venue": "Open Auditorium"
+      }
+    ]
+  }
+}
+export default sessionData;
+
+/*{
+  "feedback_questions": [
+    "How would you rate the overall organization of the hackathon?",
+    "How satisfied are you with the communication & updates (instructions, timings, announcements)?",
+    "How do you rate the venue & arrangements (seating, internet, power supply, facilities)?",
+    "How confident you are after attending the internal hackathon?",
+    "How supportive and approachable were the faculty coordinators & mentors?",
+    "Did the Jury comments and input give scope for refining or improving your idea?",
+    "Did the hackathon provide a good opportunity to showcase your skills and creativity?",
+    "How do you rate the time management & scheduling of the event?",
+    "How useful was the hackathon in terms of your learning & overall experience?",
+    "How likely are you to recommend participation in future hackathons to your peers?"
+  ],
+  "SIH": {
+    "16.09.2025": [
+      {
+        "time": "09:00-13.00",
+        "topic": "Smart India Hackathon",
+        "venue": "Open Auditorium",
+        "feedback_questions": "See above"
+      } 
+    ]
+  }
+}*/
