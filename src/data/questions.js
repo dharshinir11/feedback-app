09975418a23e570a9efde350976e01faa5142950
Ridhyadharshinni <ridhyatr@gmail.com>
2025-09-19 01:07:21 +0530

    Update questions.js

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,9 +1,13 @@
 export default {
-  Q1: "Opinion about overall session",
-  Q2: "Clarity in the speech",
-  Q3: "Speaker's interaction with the students",
-  Q4: "Did the session deliver the information you expected to receive",
-  Q5: "Was the subject matter presented effectively",
-  Q6: "Rate the speaker's knowledge",
-  Q7: "Suggestions/Questions/Feedback"
+  Q1: "How would you rate the overall organization of the hackathon?",
+  Q2: "How satisfied are you with the communication & updates (instructions, timings, announcements)?",
+  Q3: "How do you rate the venue & arrangements (seating, internet, power supply, facilities)?",
+  Q4: "How confident you are after attending the internal hackathon?",
+  Q5: "How supportive and approachable were the faculty coordinators & mentors?",
+  Q6: "Did the Jury comments and input give scope for refining or improving your idea?",
+  Q7: "Did the hackathon provide a good opportunity to showcase your skills and creativity?",
+  Q8: "How do you rate the time management & scheduling of the event?",
+  Q9: "How useful was the hackathon in terms of your learning & overall experience?",
+  Q10: "How likely are you to recommend participation in future hackathons to your peers?"
 };
+
